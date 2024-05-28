0ce7ff781841447fa7a25e1e4b00b45259cb84a0
gustavobruder <gugabruder@gmail.com>
2024-05-28 17:14:28 -0300

    adicionado trabalho 3 - parcialmente feito

---
+++ trabalho3/EditorVetorialPoligonos.cs
@@ -0,0 +1,215 @@
+﻿#define CG_Debug
+
+using System.Collections.Generic;
+using System.Linq;
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL4;
+
+namespace gcgcg
+{
+    internal class EditorVetorialPoligonos : Objeto
+    {
+        private List<Poligono> _poligonos;
+        private int _indicePoligonoSelecionado;
+        private bool _estaEditandoPoligono;
+
+        public EditorVetorialPoligonos(Objeto _paiRef, ref char _rotulo) : base(_paiRef, ref _rotulo)
+        {
+            PrimitivaTipo = PrimitiveType.Points;
+            PrimitivaTamanho = 1;
+
+            _poligonos = new List<Poligono>();
+            _indicePoligonoSelecionado = -1;
+            _estaEditandoPoligono = false;
+
+            Atualizar();
+        }
+
+        public bool EstaEditandoPoligono()
+        {
+            return _estaEditandoPoligono;
+        }
+
+        public void AdicionarNovoPoligono(Poligono poligono)
+        {
+            if (EstaEditandoPoligono())
+                return;
+
+            _poligonos.Add(poligono);
+            _indicePoligonoSelecionado = _poligonos.Count - 1;
+            _estaEditandoPoligono = true;
+
+            base.FilhoAdicionar(poligono);
+            Atualizar();
+        }
+
+        public void AdicionarPontoPoligono(Ponto4D ponto)
+        {
+            if (!EstaEditandoPoligono())
+                return;
+
+            _poligonos.Last().PontosAdicionar(ponto);
+            Atualizar();
+        }
+
+        public void FinalizarPoligono()
+        {
+            if (!EstaEditandoPoligono())
+                return;
+
+            _estaEditandoPoligono = false;
+
+            Atualizar();
+        }
+
+        public Poligono ObterPoligonoSelecionado()
+        {
+            if (_indicePoligonoSelecionado < 0 || _indicePoligonoSelecionado >= _poligonos.Count)
+                return null;
+
+            return _poligonos[_indicePoligonoSelecionado];
+        }
+
+        public Poligono SelecionarPoligono(Ponto4D pontoClique)
+        {
+            for (var indicePoligono = 0; indicePoligono < _poligonos.Count; indicePoligono++)
+            {
+                var poligono = _poligonos[indicePoligono];
+
+                var flagBBoxPoligono = poligono.Bbox().Dentro(pontoClique);
+
+                if (!flagBBoxPoligono)
+                    continue;
+
+                var pontosPoligono = poligono.pontosLista.Append(poligono.pontosLista.First()).ToArray();
+                var count = 0;
+
+                for (var indicePonto = 0; indicePonto < poligono.pontosLista.Count; indicePonto++)
+                {
+                    var p1 = pontosPoligono[indicePonto];
+                    var p2 = pontosPoligono[indicePonto + 1];
+
+                    var flagScanLine = Matematica.ScanLine(pontoClique, p1, p2);
+
+                    if (flagScanLine)
+                        count++;
+                }
+
+                if (count % 2 != 0)
+                {
+                    _indicePoligonoSelecionado = indicePoligono;
+                    return poligono;
+                }
+            }
+
+            _indicePoligonoSelecionado = -1;
+            return null;
+        }
+
+        public void AlterarPontoMaixProximoPoligonoSelecionado(Ponto4D pontoClique)
+        {
+            var poligonoSelecionado = ObterPoligonoSelecionado();
+
+            if (poligonoSelecionado == null || poligonoSelecionado.pontosLista.Count == 0)
+                return;
+
+            (double Distancia, int Indice) pontoMenorDistancia = (double.MaxValue, -1);
+
+            for (var i = 0; i < poligonoSelecionado.pontosLista.Count; i++)
+            {
+                var pontoPoligono = poligonoSelecionado.pontosLista[i];
+
+                var distancia = Matematica.Distancia(pontoClique, pontoPoligono);
+
+                if (distancia < pontoMenorDistancia.Distancia)
+                    pontoMenorDistancia = (distancia, i);
+            }
+
+            poligonoSelecionado.PontosAlterar(pontoClique, pontoMenorDistancia.Indice);
+
+            Atualizar();
+        }
+
+        public void RemoverPontoMaixProximoPoligonoSelecionado(Ponto4D pontoClique)
+        {
+            var poligonoSelecionado = ObterPoligonoSelecionado();
+
+            if (poligonoSelecionado == null || poligonoSelecionado.pontosLista.Count == 0)
+                return;
+
+            (double Distancia, int Indice) pontoMenorDistancia = (double.MaxValue, -1);
+
+            for (var i = 0; i < poligonoSelecionado.pontosLista.Count; i++)
+            {
+                var pontoPoligono = poligonoSelecionado.pontosLista[i];
+
+                var distancia = Matematica.Distancia(pontoClique, pontoPoligono);
+
+                if (distancia < pontoMenorDistancia.Distancia)
+                    pontoMenorDistancia = (distancia, i);
+            }
+
+            poligonoSelecionado.PontosRemover(pontoMenorDistancia.Indice);
+
+            Atualizar();
+        }
+
+        public void TranslacaoPoligonoSelecionado(double tx, double ty, double tz)
+        {
+            var poligonoSelecionado = ObterPoligonoSelecionado();
+
+            if (poligonoSelecionado == null)
+                return;
+
+            poligonoSelecionado.MatrizTranslacaoXYZ(tx, ty, tz);
+
+            Atualizar();
+        }
+
+        public void EscalaPoligonoSelecionado(double tx, double ty, double tz)
+        {
+            var poligonoSelecionado = ObterPoligonoSelecionado();
+
+            if (poligonoSelecionado == null)
+                return;
+
+            var cloneCentroBBox = new Ponto4D(poligonoSelecionado.Bbox().ObterCentro);
+            poligonoSelecionado.MatrizTranslacaoXYZ(-cloneCentroBBox.X, -cloneCentroBBox.Y, -cloneCentroBBox.Z);
+            poligonoSelecionado.MatrizEscalaXYZ(tx, ty, tz);
+            poligonoSelecionado.MatrizTranslacaoXYZ(cloneCentroBBox.X, cloneCentroBBox.Y, cloneCentroBBox.Z);
+
+            Atualizar();
+        }
+
+        public void RotacaoPoligonoSelecionado(double angulo)
+        {
+            var poligonoSelecionado = ObterPoligonoSelecionado();
+
+            if (poligonoSelecionado == null)
+                return;
+
+            var cloneCentroBBox = new Ponto4D(poligonoSelecionado.Bbox().ObterCentro);
+            poligonoSelecionado.MatrizTranslacaoXYZ(-cloneCentroBBox.X, -cloneCentroBBox.Y, -cloneCentroBBox.Z);
+            poligonoSelecionado.MatrizRotacao(angulo);
+            poligonoSelecionado.MatrizTranslacaoXYZ(cloneCentroBBox.X, cloneCentroBBox.Y, cloneCentroBBox.Z);
+
+            Atualizar();
+        }
+
+        private void Atualizar()
+        {
+            base.ObjetoAtualizar();
+        }
+
+#if CG_Debug
+    public override string ToString()
+    {
+        string retorno;
+        retorno = "__ Objeto EditorVetorialPoligonos _ Tipo: " + PrimitivaTipo + " _ Tamanho: " + PrimitivaTamanho + "\n";
+        retorno += base.ImprimeToString();
+        return retorno;
+    }
+#endif
+
+    }
+}
---
+++ trabalho3/Mundo.cs
@@ -0,0 +1,369 @@
+﻿#define CG_DEBUG
+#define CG_Gizmo      
+#define CG_OpenGL      
+// #define CG_OpenTK
+// #define CG_DirectX      
+#define CG_Privado  
+
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+using System;
+using OpenTK.Mathematics;
+using System.Collections.Generic;
+
+namespace gcgcg
+{
+  public class Mundo : GameWindow
+  {
+    private static Objeto mundo = null;
+    private static EditorVetorialPoligonos _editorVetorialPoligonos = null;
+
+    private char rotuloAtual = '?';
+    private Objeto objetoSelecionado = null;
+
+    private readonly float[] _sruEixos =
+    [
+       0.0f,  0.0f,  0.0f, /* X- */      0.5f,  0.0f,  0.0f, /* X+ */
+       0.0f,  0.0f,  0.0f, /* Y- */      0.0f,  0.5f,  0.0f, /* Y+ */
+       0.0f,  0.0f,  0.0f, /* Z- */      0.0f,  0.0f,  0.5f  /* Z+ */
+    ];
+
+    private int _vertexBufferObject_sruEixos;
+    private int _vertexArrayObject_sruEixos;
+
+    private int _vertexBufferObject_bbox;
+    private int _vertexArrayObject_bbox;
+
+    private Shader _shaderBranca;
+    private Shader _shaderVermelha;
+    private Shader _shaderVerde;
+    private Shader _shaderAzul;
+    private Shader _shaderCiano;
+    private Shader _shaderMagenta;
+    private Shader _shaderAmarela;
+
+    public Mundo(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+      : base(gameWindowSettings, nativeWindowSettings)
+    {
+      mundo ??= new Objeto(null, ref rotuloAtual); //padrão Singleton
+    }
+
+    protected override void OnLoad()
+    {
+      base.OnLoad();
+
+      Utilitario.Diretivas();
+#if CG_DEBUG      
+      Console.WriteLine("Tamanho interno da janela de desenho: " + ClientSize.X + "x" + ClientSize.Y);
+#endif
+
+      GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+      #region Cores
+      _shaderBranca = new Shader("Shaders/shader.vert", "Shaders/shaderBranca.frag");
+      _shaderVermelha = new Shader("Shaders/shader.vert", "Shaders/shaderVermelha.frag");
+      _shaderVerde = new Shader("Shaders/shader.vert", "Shaders/shaderVerde.frag");
+      _shaderAzul = new Shader("Shaders/shader.vert", "Shaders/shaderAzul.frag");
+      _shaderCiano = new Shader("Shaders/shader.vert", "Shaders/shaderCiano.frag");
+      _shaderMagenta = new Shader("Shaders/shader.vert", "Shaders/shaderMagenta.frag");
+      _shaderAmarela = new Shader("Shaders/shader.vert", "Shaders/shaderAmarela.frag");
+      #endregion
+
+      #region Eixos: SRU  
+      _vertexBufferObject_sruEixos = GL.GenBuffer();
+      GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject_sruEixos);
+      GL.BufferData(BufferTarget.ArrayBuffer, _sruEixos.Length * sizeof(float), _sruEixos, BufferUsageHint.StaticDraw);
+      _vertexArrayObject_sruEixos = GL.GenVertexArray();
+      GL.BindVertexArray(_vertexArrayObject_sruEixos);
+      GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+      GL.EnableVertexAttribArray(0);
+      #endregion
+
+      #region Objeto: editor vetorial poligonos
+      _editorVetorialPoligonos = new EditorVetorialPoligonos(mundo, ref rotuloAtual);
+      #endregion
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+      base.OnRenderFrame(e);
+
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+
+      mundo.Desenhar(new Transformacao4D());
+
+#if CG_Gizmo      
+      Gizmo_Sru3D();
+      Gizmo_BBox();
+#endif
+      SwapBuffers();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+      base.OnUpdateFrame(e);
+
+      #region Teclado
+
+      var estadoTeclado = KeyboardState;
+
+      if (estadoTeclado.IsKeyDown(Keys.Escape))
+      {
+        Close();
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.Enter))
+      {
+        Console.WriteLine("Tecla Enter");
+
+        _editorVetorialPoligonos.FinalizarPoligono();
+        objetoSelecionado = _editorVetorialPoligonos.ObterPoligonoSelecionado();
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.D))
+      {
+        Console.WriteLine("Tecla D");
+      }
+      if (estadoTeclado.IsKeyDown(Keys.V))
+      {
+        Console.WriteLine("Tecla V");
+
+        if (objetoSelecionado != null)
+        {
+          int janelaLargura = ClientSize.X;
+          int janelaAltura = ClientSize.Y;
+          Ponto4D mousePonto = new Ponto4D(MousePosition.X, MousePosition.Y);
+          Ponto4D sruPonto = Utilitario.NDC_TelaSRU(janelaLargura, janelaAltura, mousePonto);
+
+          _editorVetorialPoligonos.AlterarPontoMaixProximoPoligonoSelecionado(sruPonto);
+        }
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.E))
+      {
+        Console.WriteLine("Tecla E");
+
+        if (objetoSelecionado != null)
+        {
+          int janelaLargura = ClientSize.X;
+          int janelaAltura = ClientSize.Y;
+          Ponto4D mousePonto = new Ponto4D(MousePosition.X, MousePosition.Y);
+          Ponto4D sruPonto = Utilitario.NDC_TelaSRU(janelaLargura, janelaAltura, mousePonto);
+
+          _editorVetorialPoligonos.RemoverPontoMaixProximoPoligonoSelecionado(sruPonto);
+        }
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.P))
+      {
+        Console.WriteLine("Tecla P");
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.R))
+      {
+        Console.WriteLine("Tecla R");
+
+        if (objetoSelecionado != null)
+          objetoSelecionado.ShaderObjeto = _shaderVermelha;
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.G))
+      {
+        Console.WriteLine("Tecla G");
+
+        if (objetoSelecionado != null)
+          objetoSelecionado.ShaderObjeto = _shaderVerde;
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.B))
+      {
+        Console.WriteLine("Tecla B");
+
+        if (objetoSelecionado != null)
+          objetoSelecionado.ShaderObjeto = _shaderAzul;
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.Up))
+      {
+        Console.WriteLine("Tecla Cima");
+
+        _editorVetorialPoligonos.TranslacaoPoligonoSelecionado(0.0, 0.1, 0.0);
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.Down))
+      {
+        Console.WriteLine("Tecla Baixo");
+
+        _editorVetorialPoligonos.TranslacaoPoligonoSelecionado(0.0, -0.1, 0.0);
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.Right))
+      {
+        Console.WriteLine("Tecla Dir");
+
+        _editorVetorialPoligonos.TranslacaoPoligonoSelecionado(0.1, 0.0, 0.0);
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.Left))
+      {
+        Console.WriteLine("Tecla Esq");
+
+        _editorVetorialPoligonos.TranslacaoPoligonoSelecionado(-0.1, 0.0, 0.0);
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.Home))
+      {
+        Console.WriteLine("Tecla Home");
+
+        _editorVetorialPoligonos.EscalaPoligonoSelecionado(2.0, 2.0, 1.0);
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.End))
+      {
+        Console.WriteLine("Tecla End");
+
+        _editorVetorialPoligonos.EscalaPoligonoSelecionado(0.5, 0.5, 1.0);
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.D3))
+      {
+        Console.WriteLine("Tecla 3");
+
+        _editorVetorialPoligonos.RotacaoPoligonoSelecionado(10.0);
+      }
+      if (estadoTeclado.IsKeyPressed(Keys.D4))
+      {
+        Console.WriteLine("Tecla 4");
+
+        _editorVetorialPoligonos.RotacaoPoligonoSelecionado(-10.0);
+      }
+
+      #endregion
+
+      #region Mouse
+
+      if (MouseState.IsButtonPressed(MouseButton.Left))
+      {
+        Console.WriteLine("Mouse Left");
+
+        int janelaLargura = ClientSize.X;
+        int janelaAltura = ClientSize.Y;
+        Ponto4D mousePonto = new Ponto4D(MousePosition.X, MousePosition.Y);
+        Ponto4D sruPonto = Utilitario.NDC_TelaSRU(janelaLargura, janelaAltura, mousePonto);
+
+        objetoSelecionado = _editorVetorialPoligonos.SelecionarPoligono(sruPonto);
+      }
+      if (MouseState.IsButtonPressed(MouseButton.Right))
+      {
+        Console.WriteLine("Mouse Right");
+
+        if (!_editorVetorialPoligonos.EstaEditandoPoligono())
+          _editorVetorialPoligonos.AdicionarNovoPoligono(new Poligono(mundo, ref rotuloAtual, new List<Ponto4D>()));
+
+        int janelaLargura = ClientSize.X;
+        int janelaAltura = ClientSize.Y;
+        Ponto4D mousePonto = new Ponto4D(MousePosition.X, MousePosition.Y);
+        Ponto4D sruPonto = Utilitario.NDC_TelaSRU(janelaLargura, janelaAltura, mousePonto);
+
+        _editorVetorialPoligonos.AdicionarPontoPoligono(sruPonto);
+      }
+      if (MouseState.IsButtonReleased(MouseButton.Right))
+      {
+        Console.WriteLine("MouseState.IsButtonReleased(MouseButton.Right)");
+      }
+
+      #endregion
+
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+      base.OnResize(e);
+
+#if CG_DEBUG      
+      Console.WriteLine("Tamanho interno da janela de desenho: " + ClientSize.X + "x" + ClientSize.Y);
+#endif
+      GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
+    }
+
+    protected override void OnUnload()
+    {
+      mundo.OnUnload();
+
+      GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+      GL.BindVertexArray(0);
+      GL.UseProgram(0);
+
+      GL.DeleteBuffer(_vertexBufferObject_sruEixos);
+      GL.DeleteVertexArray(_vertexArrayObject_sruEixos);
+
+      GL.DeleteBuffer(_vertexBufferObject_bbox);
+      GL.DeleteVertexArray(_vertexArrayObject_bbox);
+
+      GL.DeleteProgram(_shaderBranca.Handle);
+      GL.DeleteProgram(_shaderVermelha.Handle);
+      GL.DeleteProgram(_shaderVerde.Handle);
+      GL.DeleteProgram(_shaderAzul.Handle);
+      GL.DeleteProgram(_shaderCiano.Handle);
+      GL.DeleteProgram(_shaderMagenta.Handle);
+      GL.DeleteProgram(_shaderAmarela.Handle);
+
+      base.OnUnload();
+    }
+
+#if CG_Gizmo
+    private void Gizmo_Sru3D()
+    {
+#if CG_OpenGL && !CG_DirectX
+      var transform = Matrix4.Identity;
+      GL.BindVertexArray(_vertexArrayObject_sruEixos);
+      // EixoX
+      _shaderVermelha.SetMatrix4("transform", transform);
+      _shaderVermelha.Use();
+      GL.DrawArrays(PrimitiveType.Lines, 0, 2);
+      // EixoY
+      _shaderVerde.SetMatrix4("transform", transform);
+      _shaderVerde.Use();
+      GL.DrawArrays(PrimitiveType.Lines, 2, 2);
+      // EixoZ
+      _shaderAzul.SetMatrix4("transform", transform);
+      _shaderAzul.Use();
+      GL.DrawArrays(PrimitiveType.Lines, 4, 2);
+#elif CG_DirectX && !CG_OpenGL
+      Console.WriteLine(" .. Coloque aqui o seu código em DirectX");
+#elif (CG_DirectX && CG_OpenGL) || (!CG_DirectX && !CG_OpenGL)
+      Console.WriteLine(" .. ERRO de Render - escolha OpenGL ou DirectX !!");
+#endif
+    }
+#endif    
+
+#if CG_Gizmo
+    private void Gizmo_BBox()   //FIXME: não é atualizada com as transformações globais
+    {
+      if (objetoSelecionado != null)
+      {
+
+#if CG_OpenGL && !CG_DirectX
+
+        float[] _bbox =
+        {
+        (float) objetoSelecionado.Bbox().ObterMenorX, (float) objetoSelecionado.Bbox().ObterMenorY, 0.0f, // A
+        (float) objetoSelecionado.Bbox().ObterMaiorX, (float) objetoSelecionado.Bbox().ObterMenorY, 0.0f, // B
+        (float) objetoSelecionado.Bbox().ObterMaiorX, (float) objetoSelecionado.Bbox().ObterMaiorY, 0.0f, // C
+        (float) objetoSelecionado.Bbox().ObterMenorX, (float) objetoSelecionado.Bbox().ObterMaiorY, 0.0f  // D
+      };
+
+        _vertexBufferObject_bbox = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject_bbox);
+        GL.BufferData(BufferTarget.ArrayBuffer, _bbox.Length * sizeof(float), _bbox, BufferUsageHint.StaticDraw);
+        _vertexArrayObject_bbox = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject_bbox);
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+
+        var transform = Matrix4.Identity;
+        GL.BindVertexArray(_vertexArrayObject_bbox);
+        _shaderAmarela.SetMatrix4("transform", transform);
+        _shaderAmarela.Use();
+        GL.DrawArrays(PrimitiveType.LineLoop, 0, (_bbox.Length / 3));
+
+#elif CG_DirectX && !CG_OpenGL
+      Console.WriteLine(" .. Coloque aqui o seu código em DirectX");
+#elif (CG_DirectX && CG_OpenGL) || (!CG_DirectX && !CG_OpenGL)
+      Console.WriteLine(" .. ERRO de Render - escolha OpenGL ou DirectX !!");
+#endif
+      }
+    }
+#endif    
+
+  }
+}
